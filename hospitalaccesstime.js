ff3f1817db54cf78207f0a21ea583453a1be8db6
Chaitrika Budamagunta <cbudamagunta@dons.usfca.edu>
2020-03-14 21:47:24 -0700

    Add linking logic to heatmap mouse events

--- hospitalaccesstime.js
+++ hospitalaccesstime.js
@@ -5,237 +5,8 @@
 let activeNeighborhood;
 let formatter = d3.format(".2f");
 
-/*******************************************************************************/
-
-
-/*
-* HEATMAP
-*/
-
-const heatMargin = {
-  top: 120,
-  bottom: 10,
-  left: 160,
-  right: 10
-};
-
-
-/* PLOT */
-let heatSvg = d3.select("body").select("svg#HeatVis");
-const heatPlot = heatSvg.append("g").attr("id", "plot");
-
-heatPlot.attr("transform", "translate(" + heatMargin.left + "," + heatMargin.top + ")");
-
-
-/* SCALES */
-let heatBounds = heatSvg.node().getBoundingClientRect();
-let heatPlotWidth = heatBounds.width - heatMargin.right - heatMargin.left;
-let heatPlotHeight = heatBounds.height - heatMargin.top - heatMargin.bottom;
-
-const heatScales = {
-  x: d3.scaleBand(),
-  y: d3.scaleBand(),
-  color: d3.scaleSequential(d3.interpolateBlues)
-};
-
-heatScales.x.range([750 - heatMargin.left - heatMargin.right, 0]);
-heatScales.y.range([800 - heatMargin.top - heatMargin.bottom, 0]);
-
-heatScales.color.domain([20, 110]);
-
-
-/* PLOT SETUP */
-drawHeatTitles();
-drawHeatLegend();
-
-
-/* LOAD THE DATA */
-d3.csv("data/Hospital_Access_Heatmap_Data.csv", parseHeatmapData).then(drawHeatmap);
-
-
-/* AXIS TITLES */
-function drawHeatTitles() {
-
-    const xMiddle = heatMargin.left + midpoint(heatScales.x.range());
-    const yMiddle = heatMargin.top + midpoint(heatScales.y.range());
-
-    const xTitleGroup = heatSvg.append('g');
-    const xTitle = xTitleGroup.append('text')
-      .attr('class', 'axis-title')
-      .attr("id", "axis-title")
-      .text('Call Type Groups');
-
-    xTitle.attr('x', xMiddle);
-    xTitle.attr('y', 0 + heatMargin.top - 18);
-    xTitle.attr('dy', -4);
-    xTitle.attr('text-anchor', 'middle');
-
-    const yTitleGroup = heatSvg.append('g');
-    yTitleGroup.attr('transform', translate(4, yMiddle));
-
-    const yTitle = yTitleGroup.append('text')
-      .attr('class', 'axis-title')
-      .attr("id", "axis-title")
-      .text('Neighborhoods');
-
-    yTitle.attr('x', 0);
-    yTitle.attr('y', 0);
-
-    yTitle.attr('dy', -353);
-    yTitle.attr('dx', 0);
-}
-
-
-/* LEGEND */
-function drawHeatLegend(){
-
-  const legendWidth = 300;
-  const legendHeight = 25;
-
-  const colorGroup = heatSvg.append('g').attr('id', 'color-legend');
-  colorGroup.attr('transform', translate(18, heatMargin.top - 110));
-
-  const title = colorGroup.append('text')
-    .attr('class', 'legend-title')
-    .attr('id', 'legend-title')
-    .text('Time between Recieving Call and Arrival at Hospital (Minutes)');
-
-  title.attr('dy', 12);
-
-  const colorbox = colorGroup.append('rect')
-    .attr('x', 0)
-    .attr('y', 12 + 6)
-    .attr('width', legendWidth)
-    .attr('height', legendHeight);
-
-  const colorDomain = [d3.min(heatScales.color.domain()), d3.max(heatScales.color.domain())];
-  heatScales.percent = d3.scaleLinear()
-    .range([0, 100])
-    .domain(colorDomain);
-
-  const defs = heatSvg.append('defs');
-
-  defs.append('linearGradient')
-    .attr('id', 'gradient')
-    .selectAll('stop')
-    .data(heatScales.color.ticks())
-    .enter()
-    .append('stop')
-    .attr('offset', d => heatScales.percent(d) + '%')
-    .attr('stop-color', d => heatScales.color(d));
-
-  colorbox.attr('fill', 'url(#gradient)');
-
-  heatScales.legend = d3.scaleLinear()
-    .domain(colorDomain)
-    .range([0, legendWidth]);
-
-  const legendAxis = d3.axisBottom(heatScales.legend)
-    .tickValues(heatScales.color.domain())
-    .tickSize(legendHeight)
-    .tickSizeOuter(0);
-
-  const axisGroup = colorGroup.append('g')
-    .attr('id', 'color-axis')
-    .attr('transform', translate(0, 12 + 6))
-    .call(legendAxis);
-}
-
-
-/*
-* Draw the heatmap
-*/
-function drawHeatmap(data) {
-
-  // data = data.sort(function(a, b) {
-  //   return a['recievingToHospital'] - b['recievingToHospital'];
-  // });
-
-  /* DRAW AXIS */
-  let neighborhoods = data.map(row => row.neighborhoods);
-  heatScales.y.domain(neighborhoods);
-
-  let callTypeGroups = data.map(row => row.callType);
-  heatScales.x.domain(callTypeGroups);
-
-  let xGroup = heatPlot.append("g").attr("id", "x-axis-heat").attr('class', 'axis');
-  let yGroup = heatPlot.append("g").attr("id", "y-axis-heat").attr('class', 'axis');
-
-  let xAxis = d3.axisTop(heatScales.x).tickPadding(0).tickSizeOuter(0);
-  let yAxis = d3.axisRight(heatScales.y).tickPadding(0).tickSizeOuter(0);
-
-  xGroup.attr('transform', translate(0, heatMargin.top - 120));
-  xGroup.call(xAxis);
-
-  yGroup.attr('transform', translate(-160, 0));
-  yGroup.call(yAxis);
-
-  /* CREATE CELLS */
-  let rows = heatPlot.selectAll("g.cell")
-    .data(data)
-    .enter()
-    .append("g");
-
-  rows.attr("class", "cell");
-  rows.attr("id", d => d.neighborhoods);
-
-  rows.attr("transform", function(d) {
-    return translate(0, heatScales.y(d.neighborhoods));
-  });
-
-  let cells = rows.selectAll("rect")
-    .data(data)
-    .attr("id", "cells")
-    .enter()
-    .append("rect")
-      .attr("class", d => d.neighborhoods)
-      .attr("x", d => heatScales.x(d.callType))
-      .attr("y", d => heatScales.y(d.neighborhoods))
-      .attr("width", heatScales.x.bandwidth())
-      .attr("height", heatScales.y.bandwidth())
-
-      /* remove null values */
-      .style("fill", "white")
-      .filter(d => d.recievingToHospital >= 0)
-
-      /* color in rest of the cells */
-      .style("fill", d => heatScales.color(d.recievingToHospital))
-      .style("stroke", d => heatScales.color(d.recievingToHospital));
-
-  cells.
-    on("mouseover", function(d) {
-      let tooltip = formatter(d.recievingToHospital) + " minutes";
-
-      rows.append("text")
-        .attr("id", "heatNumber")
-        .attr("x", heatScales.x(d.callType))
-        .attr("y", heatScales.y(d.neighborhoods) + 12)
-        .attr("text-anchor", "start")
-        .attr("font-size", "12px")
-        .style("fill", "black")
-        .style("font-weight", "bold")
-        .text(tooltip);
-    })
-    .on("mouseout", function(d) {
-      d3.select("#heatNumber").remove();
-    });
-}
-
-
-/*
- * Convert values as necessary and discard unused columns
- */
-function parseHeatmapData(row){
-
-  let keep = {};
-
-  keep.callType = row["Call Type Group"];
-  keep.neighborhoods = row["Neighborhooods"];
-  keep.recievingToHospital = parseFloat(row["Avg. Recieving Call to Hospital"]);
-
-  return keep;
-}
-
+var barData;
+var lineData;
 
 /*******************************************************************************/
 
@@ -499,71 +270,299 @@ function parseBarLineData(row){
 
 /*******************************************************************************/
 
-linking();
+
+
+/*
+* HEATMAP
+*/
+
+const heatMargin = {
+  top: 120,
+  bottom: 10,
+  left: 160,
+  right: 10
+};
+
+
+/* PLOT */
+let heatSvg = d3.select("body").select("svg#HeatVis");
+const heatPlot = heatSvg.append("g").attr("id", "plot");
+
+heatPlot.attr("transform", "translate(" + heatMargin.left + "," + heatMargin.top + ")");
+
+
+/* SCALES */
+let heatBounds = heatSvg.node().getBoundingClientRect();
+let heatPlotWidth = heatBounds.width - heatMargin.right - heatMargin.left;
+let heatPlotHeight = heatBounds.height - heatMargin.top - heatMargin.bottom;
+
+const heatScales = {
+  x: d3.scaleBand(),
+  y: d3.scaleBand(),
+  color: d3.scaleSequential(d3.interpolateBlues)
+};
+
+heatScales.x.range([750 - heatMargin.left - heatMargin.right, 0]);
+heatScales.y.range([800 - heatMargin.top - heatMargin.bottom, 0]);
+
+heatScales.color.domain([20, 110]);
+
+
+/* PLOT SETUP */
+drawHeatTitles();
+drawHeatLegend();
+
+
+/* LOAD THE DATA */
+d3.csv("data/Hospital_Access_Heatmap_Data.csv", parseHeatmapData).then(drawHeatmap);
+
+
+/* AXIS TITLES */
+function drawHeatTitles() {
+
+    const xMiddle = heatMargin.left + midpoint(heatScales.x.range());
+    const yMiddle = heatMargin.top + midpoint(heatScales.y.range());
+
+    const xTitleGroup = heatSvg.append('g');
+    const xTitle = xTitleGroup.append('text')
+      .attr('class', 'axis-title')
+      .attr("id", "axis-title")
+      .text('Call Type Groups');
+
+    xTitle.attr('x', xMiddle);
+    xTitle.attr('y', 0 + heatMargin.top - 18);
+    xTitle.attr('dy', -4);
+    xTitle.attr('text-anchor', 'middle');
+
+    const yTitleGroup = heatSvg.append('g');
+    yTitleGroup.attr('transform', translate(4, yMiddle));
+
+    const yTitle = yTitleGroup.append('text')
+      .attr('class', 'axis-title')
+      .attr("id", "axis-title")
+      .text('Neighborhoods');
+
+    yTitle.attr('x', 0);
+    yTitle.attr('y', 0);
+
+    yTitle.attr('dy', -353);
+    yTitle.attr('dx', 0);
+}
+
+
+/* LEGEND */
+function drawHeatLegend(){
+
+  const legendWidth = 300;
+  const legendHeight = 25;
+
+  const colorGroup = heatSvg.append('g').attr('id', 'color-legend');
+  colorGroup.attr('transform', translate(18, heatMargin.top - 110));
+
+  const title = colorGroup.append('text')
+    .attr('class', 'legend-title')
+    .attr('id', 'legend-title')
+    .text('Time between Recieving Call and Arrival at Hospital (Minutes)');
+
+  title.attr('dy', 12);
+
+  const colorbox = colorGroup.append('rect')
+    .attr('x', 0)
+    .attr('y', 12 + 6)
+    .attr('width', legendWidth)
+    .attr('height', legendHeight);
+
+  const colorDomain = [d3.min(heatScales.color.domain()), d3.max(heatScales.color.domain())];
+  heatScales.percent = d3.scaleLinear()
+    .range([0, 100])
+    .domain(colorDomain);
+
+  const defs = heatSvg.append('defs');
+
+  defs.append('linearGradient')
+    .attr('id', 'gradient')
+    .selectAll('stop')
+    .data(heatScales.color.ticks())
+    .enter()
+    .append('stop')
+    .attr('offset', d => heatScales.percent(d) + '%')
+    .attr('stop-color', d => heatScales.color(d));
+
+  colorbox.attr('fill', 'url(#gradient)');
+
+  heatScales.legend = d3.scaleLinear()
+    .domain(colorDomain)
+    .range([0, legendWidth]);
+
+  const legendAxis = d3.axisBottom(heatScales.legend)
+    .tickValues(heatScales.color.domain())
+    .tickSize(legendHeight)
+    .tickSizeOuter(0);
+
+  const axisGroup = colorGroup.append('g')
+    .attr('id', 'color-axis')
+    .attr('transform', translate(0, 12 + 6))
+    .call(legendAxis);
+}
+
+
+/*
+* Draw the heatmap
+*/
+function drawHeatmap(data) {
+
+  // data = data.sort(function(a, b) {
+  //   return a['recievingToHospital'] - b['recievingToHospital'];
+  // });
+
+  /* DRAW AXIS */
+  let neighborhoods = data.map(row => row.neighborhoods);
+  heatScales.y.domain(neighborhoods);
+
+  let callTypeGroups = data.map(row => row.callType);
+  heatScales.x.domain(callTypeGroups);
+
+  let xGroup = heatPlot.append("g").attr("id", "x-axis-heat").attr('class', 'axis');
+  let yGroup = heatPlot.append("g").attr("id", "y-axis-heat").attr('class', 'axis');
+
+  let xAxis = d3.axisTop(heatScales.x).tickPadding(0).tickSizeOuter(0);
+  let yAxis = d3.axisRight(heatScales.y).tickPadding(0).tickSizeOuter(0);
+
+  xGroup.attr('transform', translate(0, heatMargin.top - 120));
+  xGroup.call(xAxis);
+
+  yGroup.attr('transform', translate(-160, 0));
+  yGroup.call(yAxis);
+
+  /* CREATE CELLS */
+  let rows = heatPlot.selectAll("g.cell")
+    .data(data)
+    .enter()
+    .append("g");
+
+  rows.attr("class", "cell");
+  rows.attr("id", d => d.neighborhoods);
+
+  rows.attr("transform", function(d) {
+    return translate(0, heatScales.y(d.neighborhoods));
+  });
+
+  let cells = rows.selectAll("rect")
+    .data(data)
+    .attr("id", "cells")
+    .enter()
+    .append("rect")
+      .attr("class", d => d.neighborhoods)
+      .attr("x", d => heatScales.x(d.callType))
+      .attr("y", d => heatScales.y(d.neighborhoods))
+      .attr("width", heatScales.x.bandwidth())
+      .attr("height", heatScales.y.bandwidth())
+
+      /* remove null values */
+      .style("fill", "white")
+      .filter(d => d.recievingToHospital >= 0)
+
+      /* color in rest of the cells */
+      .style("fill", d => heatScales.color(d.recievingToHospital))
+      .style("stroke", d => heatScales.color(d.recievingToHospital));
+
+
+
+    const bars = d3.select("barLineGroup")
+      .select("bars");
+
+    cells.
+      on("mouseover", function(d) {
+        let tooltip = formatter(d.recievingToHospital) + " minutes";
+
+        rows.append("text")
+          .attr("id", "heatNumber")
+          .attr("x", heatScales.x(d.callType))
+          .attr("y", heatScales.y(d.neighborhoods) + 12)
+          .attr("text-anchor", "start")
+          .attr("font-size", "12px")
+          .style("fill", "black")
+          .style("font-weight", "bold")
+          .text(tooltip);
+
+
+        /* Linking to bar chart? */
+        bars.filter(e => (d.neighborhoods !== e.neighborhoods))
+          .transition()
+          .style("fill", "#bbbbbb");
+
+        let lineMatch = lineData.filter(e => e.neighborhoods === d.neighborhoods);
+
+        let tooltip1 = "On Scene to Hospital: " + formatter(d.time) + " minutes";
+        let tooltip2 = "Recieving Call to On Scene: " + formatter(lineMatch[0].time) + " minutes";
+
+        bars.append("text")
+          .attr("id", "heatBarTooltipN")
+          .attr("x", barLineBounds.width - barLineMargin.right - 50)
+          .attr("y", -75)
+          .attr("text-anchor", "end")
+          .attr("font-size", "12px")
+          .style("fill", "395d87")
+          .style("font-weight", "bold")
+          .text(d.neighborhoods);
+
+        bars.append("text")
+          .attr("id", "heatBarTooltip1")
+          .attr("x", barLineBounds.width - barLineMargin.right - 50)
+          .attr("y", -35)
+          .attr("text-anchor", "end")
+          .attr("font-size", "12px")
+          .style("fill", "395d87")
+          .text(tooltip1);
+
+        bars.append("text")
+          .attr("id", "heatBarTooltip2")
+          .attr("x", barLineBounds.width - barLineMargin.right - 50)
+          .attr("y", -55)
+          .attr("text-anchor", "end")
+          .attr("font-size", "12px")
+          .style("fill", "395d87")
+          .text(tooltip2);
+
+
+    })
+    .on("mouseout", function(d) {
+
+      d3.select("#heatNumber").remove();
+
+
+      /* Linking to bar chart? */
+      bars.style("fill", "a3c7e1");
+
+      d3.select("#heatBarTooltipN").remove();
+      d3.select("#heatBarTooltip1").remove();
+      d3.select("#heatBarTooltip2").remove();
+    });
+}
+
+
+/*
+ * Convert values as necessary and discard unused columns
+ */
+function parseHeatmapData(row){
+
+  let keep = {};
+
+  keep.callType = row["Call Type Group"];
+  keep.neighborhoods = row["Neighborhooods"];
+  keep.recievingToHospital = parseFloat(row["Avg. Recieving Call to Hospital"]);
+
+  return keep;
+}
+
+
+/*******************************************************************************/
+
 
 /*
 * SHARED FUNCTIONS
 */
 
-function linking() {
-  const bars = d3.select("barLineGroup")
-      .select("bars");
-
-  const cells = d3.select("rows")
-    .select("cells");
-
-
-  cells.on("mouseover", function(d) {
-
-    bars.filter(e => e.neighborhoods == d.neighborhoods)
-    bars.filter(e => (d.neighborhoods !== e.neighborhoods))
-      .transition()
-      .style("fill", "#bbbbbb");
-
-    let lineMatch = lineData.filter(e => e.neighborhoods === d.neighborhoods);
-
-    let tooltip1 = "On Scene to Hospital: " + formatter(d.time) + " minutes";
-    let tooltip2 = "Recieving Call to On Scene: " + formatter(lineMatch[0].time) + " minutes";
-
-    barLineGroup.append("text")
-      .attr("id", "barTooltipN")
-      .attr("x", barLineBounds.width - barLineMargin.right - 50)
-      .attr("y", -75)
-      .attr("text-anchor", "end")
-      .attr("font-size", "12px")
-      .style("fill", "395d87")
-      .style("font-weight", "bold")
-      .text(d.neighborhoods);
-
-    barLineGroup.append("text")
-      .attr("id", "barTooltip1")
-      .attr("x", barLineBounds.width - barLineMargin.right - 50)
-      .attr("y", -35)
-      .attr("text-anchor", "end")
-      .attr("font-size", "12px")
-      .style("fill", "395d87")
-      .text(tooltip1);
-
-    barLineGroup.append("text")
-      .attr("id", "barTooltip2")
-      .attr("x", barLineBounds.width - barLineMargin.right - 50)
-      .attr("y", -55)
-      .attr("text-anchor", "end")
-      .attr("font-size", "12px")
-      .style("fill", "395d87")
-      .text(tooltip2);
-  });
-
-  cells.on("mouseout", function(d) {
-
-    bars.filter(e => e.neighborhoods == d.neighborhoods)
-    bars.style("fill", "a3c7e1");
-
-    d3.select("#barTooltipN").remove();
-    d3.select("#barTooltip1").remove();
-    d3.select("#barTooltip2").remove();
-  });
-}
 
 /*
  * From bubble.js example:
